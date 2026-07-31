44ab4a10bfed3555cf40051e8244d9cb60a7e9d5
africangirl <117860194+africangirl@users.noreply.github.com>
2026-07-31 14:27:26 +0000

    Update first.sh

--- first.sh
+++ first.sh
@@ -1,6 +1,9 @@
+#!/bin/bash
+
 echo "Hello from Jenkins, this is an update"
 echo "Running DevOps Builds ..."
 echo "Starting ..."
 echo "1, 2, 3 ..."
 date
 whoami
+echo "Jenkins received my webhook"
